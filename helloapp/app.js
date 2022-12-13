af5909cc2feede3c73e13a25c266c59ec3e20e9b
Petr1221 <120395181+Petr1221@users.noreply.github.com>
2022-12-13 18:16:13 +0000

    first commit

---
+++ helloapp/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+ 
+const app = express();
+app.get("/", function(request, response){
+    let name = process.argv[2];
+    let age = process.argv[3];
+    response.send("<h1>Главная страница</h1><p>"+ name+","+ age+"</p>");
+});
+app.listen(3000);
+
+let nodePath = process.argv[0];
+let appPath = process.argv[1];
+let name = process.argv[2];
+let age = process.argv[3];
+ 
+console.log("nodePath: " + nodePath);
+console.log("appPath: " + appPath);
+console.log();
+console.log("name: " + name);
+console.log("age: " + age);
